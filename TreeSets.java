f55ea71a27021196db8f9c2e40535184ea59177c
chdkumar <chokkakulakumar1123@gmail.com>
2024-08-04 11:55:05 +0530

    Set practice Example

--- TreeSets.java
+++ TreeSets.java
@@ -1,5 +1,25 @@
+import java.util.TreeSet;
+import java.util.Set;
 public class TreeSets{
 public static void main(String[]args){
 // Prepare an 3 TreeSet Objects and Load Integer,Double and String On Each Object Iterate them using an Iterator
+Set<Integer> set = new TreeSet<Integer>();
+set.add(10);
+set.add(20);
+Set<Double> set1 = new TreeSet<Double>();
+set1.add(123.235);
+set1.add(440.235);
+Set<String> set2 = new TreeSet<String>();
+set2.add("Kumar");
+set2.add("Lucky");
+for(Integer i:set){
+System.out.println(i);
+}
+for(Double j:set1){
+System.out.println(j);
+}
+for(String k:set2){
+System.out.println(k);
+}
 }
 }
