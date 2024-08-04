1fe86edfd05285e6bbe40e19004107fcc285e0fa
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-04 21:58:11 +0530

    completed

--- HashSets.java
+++ HashSets.java
@@ -1,5 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
 public class HashSets{
 public static void main(String[]args){
-// Prepare an 3 HashSet Objects and Load Integer,Double and String On Each Object Iterate them using an Iterator
+    Set<Integer> set1 = new HashSet<Integer>();
+    
+   
+    set1.add(1);
+    set1.add(2);
+    set1.add(3);
+
+    Set<Double> set2 = new HashSet<Double>();
+
+    set2.add(2.7);
+    set2.add(2.8);
+    set2.add(2.9);
+
+    Set<String> set3 = new HashSet<String>();
+
+    set3.add("Hello");
+    set3.add("World");
+    set3.add("Java");
+
+    for(int i:set1){
+        System.out.println(i);
+    }
+    for(Double i:set2){
+        System.out.println(i);
+    }
+    for(String i:set3){
+        System.out.println(i);
+    }
+
+
+
+
 }
 }
