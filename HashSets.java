74e0f8d880c366265d71a00c7809e48c55c2503e
kashifakibria <kashifakibria@gmail.com>
2024-08-08 00:33:47 +0530

    Done

--- HashSets.java
+++ HashSets.java
@@ -1,5 +1,38 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class HashSets{
-public static void main(String[]args){
+    public static void main(String[]args){
 // Prepare an 3 HashSet Objects and Load Integer,Double and String On Each Object Iterate them using an Iterator
+
+        HashSet<Integer> intSet = new HashSet<>();
+        HashSet<Double> doubleSet = new HashSet<>();
+        HashSet<String> stringSet = new HashSet<>();
+
+        intSet.add(10);
+        intSet.add(01);
+
+        doubleSet.add(4.1);
+        doubleSet.add(2.2);
+
+        stringSet.add("Kashifa");
+        stringSet.add("Aa");
+
+        Iterator<Integer> intIterator= intSet.iterator();
+        while(intIterator.hasNext()){
+            System.out.println(intIterator.next());
+        }
+
+        Iterator<Double> doubleIterator= doubleSet.iterator();
+        while(doubleIterator.hasNext()){
+            System.out.println(doubleIterator.next());
+        }
+
+        Iterator<String> stringIterator= stringSet.iterator();
+        while(stringIterator.hasNext()){
+            System.out.println(stringIterator.next());
+        }
+
+
 }
 }
--- TreeSets.java
+++ TreeSets.java
@@ -1,5 +1,40 @@
+import java.util.TreeSet;
+import java.util.Iterator;
+
 public class TreeSets{
-public static void main(String[]args){
+    public static void main(String[]args){
 // Prepare an 3 TreeSet Objects and Load Integer,Double and String On Each Object Iterate them using an Iterator
+
+        TreeSet<Integer> intSet = new TreeSet<>();  
+        TreeSet<Double> doubleSet = new TreeSet<>();
+        TreeSet<String> stringSet = new TreeSet<>();  
+        
+        intSet.add(20);
+        intSet.add(10);
+        intSet.add(30);
+        
+        doubleSet.add(1.1);
+        doubleSet.add(2.2);
+        doubleSet.add(1.3);
+
+        stringSet.add("Zz");
+        stringSet.add("Abc");
+        stringSet.add("Aa");
+        
+        Iterator<Integer> intIterator= intSet.iterator();
+        while(intIterator.hasNext()){
+            System.out.println(intIterator.next());
+        }
+        
+        Iterator<Double> doubleIterator= doubleSet.iterator();
+        while(doubleIterator.hasNext()){
+            System.out.println(doubleIterator.next());
+        }
+        
+        Iterator<String> stringIterator= stringSet.iterator();
+        while(stringIterator.hasNext()){
+            System.out.println(stringIterator.next());
+        }
+
 }
 }
