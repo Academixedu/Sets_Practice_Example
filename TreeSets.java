f4470b9f87f048741abb6f850ba3a745174a2d15
medhasR25 <kovvurumedhas@gmail.com>
2024-08-09 14:33:21 +0530

    done

--- TreeSets.java
+++ TreeSets.java
@@ -1,5 +1,46 @@
-public class TreeSets{
-public static void main(String[]args){
-// Prepare an 3 TreeSet Objects and Load Integer,Double and String On Each Object Iterate them using an Iterator
-}
+import java.util.TreeSet;
+import java.util.Iterator;
+
+public class TreeSets {
+    public static void main(String[] args) {
+        
+        TreeSet<Integer> integerSet = new TreeSet<>();
+        TreeSet<Double> doubleSet = new TreeSet<>();
+        TreeSet<String> stringSet = new TreeSet<>();
+
+        
+        integerSet.add(50);
+        integerSet.add(20);
+        integerSet.add(30);
+
+       
+        doubleSet.add(5.5);
+        doubleSet.add(2.2);
+        doubleSet.add(3.3);
+
+       
+        stringSet.add("Banana");
+        stringSet.add("Apple");
+        stringSet.add("Cherry");
+
+        
+        System.out.println("Integer TreeSet:");
+        Iterator<Integer> intIterator = integerSet.iterator();
+        while (intIterator.hasNext()) {
+            System.out.println(intIterator.next());
+        }
+
+       
+        System.out.println("\nDouble TreeSet:");
+        Iterator<Double> doubleIterator = doubleSet.iterator();
+        while (doubleIterator.hasNext()) {
+            System.out.println(doubleIterator.next());
+        }
+
+        System.out.println("\nString TreeSet:");
+        Iterator<String> stringIterator = stringSet.iterator();
+        while (stringIterator.hasNext()) {
+            System.out.println(stringIterator.next());
+        }
+    }
 }
