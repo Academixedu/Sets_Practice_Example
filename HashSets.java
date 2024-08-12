d1345964834a0f6ef96603a7812839346e9bcad1
manicharanchindam <manicharanchindam@email.com>
2024-08-12 18:38:06 +0530

    sets done

--- HashSets.java
+++ HashSets.java
@@ -1,5 +1,40 @@
-public class HashSets{
-public static void main(String[]args){
-// Prepare an 3 HashSet Objects and Load Integer,Double and String On Each Object Iterate them using an Iterator
-}
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSets {
+    public static void main(String[] args) {
+        HashSet<Integer> integerSet = new HashSet<>();
+        HashSet<Double> doubleSet = new HashSet<>();
+        HashSet<String> stringSet = new HashSet<>();
+
+        integerSet.add(1);
+        integerSet.add(2);
+        integerSet.add(3);
+
+        doubleSet.add(1.1);
+        doubleSet.add(2.2);
+        doubleSet.add(3.3);
+
+        stringSet.add("Mani");
+        stringSet.add("Charan");
+        stringSet.add("Chindam");
+
+        System.out.println("Integer HashSet:");
+        Iterator<Integer> intIterator = integerSet.iterator();
+        while (intIterator.hasNext()) {
+            System.out.println(intIterator.next());
+        }
+
+        System.out.println("\nDouble HashSet:");
+        Iterator<Double> doubleIterator = doubleSet.iterator();
+        while (doubleIterator.hasNext()) {
+            System.out.println(doubleIterator.next());
+        }
+
+        System.out.println("\nString HashSet:");
+        Iterator<String> stringIterator = stringSet.iterator();
+        while (stringIterator.hasNext()) {
+            System.out.println(stringIterator.next());
+        }
+    }
 }
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,3 +1,37 @@
-public class LinkedHashSets{
-// Prepare 3 Linked HashSet Object and Load them With Integer String and Double on Each Object Iterate them by using an Iterator and While Loop
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class LinkedHashSets {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> integerSet = new LinkedHashSet<>();
+        LinkedHashSet<String> stringSet = new LinkedHashSet<>();
+        LinkedHashSet<Double> doubleSet = new LinkedHashSet<>();
+
+        integerSet.add(10);
+        integerSet.add(20);
+        integerSet.add(30);
+
+        stringSet.add("Mani");
+        stringSet.add("Charan");
+        stringSet.add("Chindam");
+
+        doubleSet.add(1.1);
+        doubleSet.add(2.2);
+        doubleSet.add(3.3);
+
+        Iterator<Integer> intIterator = integerSet.iterator();
+        while (intIterator.hasNext()) {
+            System.out.println(intIterator.next());
+        }
+
+        Iterator<String> stringIterator = stringSet.iterator();
+        while (stringIterator.hasNext()) {
+            System.out.println(stringIterator.next());
+        }
+
+        Iterator<Double> doubleIterator = doubleSet.iterator();
+        while (doubleIterator.hasNext()) {
+            System.out.println(doubleIterator.next());
+        }
+    }
 }
